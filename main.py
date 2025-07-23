9e1935863fca7153ef981bdd4643756784dcb375
Mitia Jessica <mitiah.jessica@gmail.com>
2025-07-23 22:21:24 +0300

    modify code bases

--- main.py
+++ main.py
@@ -20,35 +20,3 @@ def read_hello(request: Request, is_teacher: bool = None, name: str = "Non défi
     else:
         return JSONResponse({"message": f"Hello {name}"}, status_code=200)
 
-
-class WelcomeRequest(BaseModel):
-    name: str
-
-
-@app.post("/welcome")
-def welcome_user(request: WelcomeRequest):
-    return {f"Bienvenue {request.name}"}
-
-
-class SecretPayload(BaseModel):
-    secret_code: int
-
-
-@app.put("/top-secret")
-def put_top_secret(request: Request, request_body: SecretPayload):
-    auth_header = request.headers.get("Authorization")
-    if auth_header != "my-secret-key":
-        return JSONResponse(
-            status_code=403,
-            content={"error": f"Unauthorized header received: {auth_header}"}
-        )
-
-    secret_code = request_body.secret_code
-    code_length = len(str(secret_code))
-    if code_length != 4:
-        return JSONResponse(
-            status_code=400,
-            content={"error": f"Le code fourni n’est pas à 4 chiffres mais {code_length} chiffres."}
-        )
-
-    return JSONResponse(content={"message": f"Voici le code {secret_code}"}, status_code=200)
